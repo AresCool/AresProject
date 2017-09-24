b0f053c40d2626c457032e1313e84501d2448c90
AresCool <847905191@qq.com>
2017-09-24 17:11:34 +0800

    修改下方法

--- bselibrary/src/main/java/com/ares/xq/baselibrary/base/AppActivity.java
+++ bselibrary/src/main/java/com/ares/xq/baselibrary/base/AppActivity.java
@@ -1,9 +1,6 @@
 package com.ares.xq.baselibrary.base;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import com.ares.xq.baselibrary.utils.AppManager;
 
 /**
  * Created by yinyinchuan on 2017/9/24.

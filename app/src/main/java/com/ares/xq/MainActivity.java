2a9d9d01619f377aa19ec79e6854ee1dd138e1fc
Administrator <847905191@qq.com>
2017-10-25 16:05:14 +0800

    新增圆形头像

--- app/src/main/java/com/ares/xq/MainActivity.java
+++ app/src/main/java/com/ares/xq/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 
 import com.ares.xq.activity.GlideActivity;
 import com.ares.xq.activity.RecListActivity;
+import com.ares.xq.activity.RoundActivity;
 import com.ares.xq.activity.TouchActivity;
 import com.ares.xq.adapter.MainAdapter;
 import com.ares.xq.baselibrary.base.activity.AppActivity;
@@ -65,8 +66,11 @@ public class MainActivity extends AppActivity implements MainAdapter.MainAdapter
             case 1://Glide 加载图片
                 intent.setClass(this,GlideActivity.class);
                 break;
-            case 2:
+            case 2://列表
                 intent.setClass(this,RecListActivity.class);
+                break;
+            case 3:
+                intent.setClass(this,RoundActivity.class);
                 break;
             default:
                 break;
---
+++ app/src/main/java/com/ares/xq/activity/RoundActivity.java
@@ -0,0 +1,18 @@
+package com.ares.xq.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ares.xq.R;
+
+/**
+ * 原图图片
+ */
+public class RoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round);
+    }
+}
